56e9c2912bd3c5d4bbdd074e9ea333e39a7ce6be
João Felipe de Lima <joao.lima@levva.io>
2023-07-09 12:27:45 -0300

    feat: Adicionado o botão de "+" feat: Envolvendo o input + botão numa "view" feat: Aplicando propriedade "flexDirection: row"

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,20 +1,33 @@
 import React from "react"
-import { StatusBar, Text, View, TextInput } from "react-native"
+import { StatusBar, Text, View, TextInput, TouchableOpacity } from "react-native"
 
 import { styles } from "./styles"
 
 export function Home() {
+
+    function handleParticipantAdd() {
+        console.log("você clicou no botão")
+    }
+
     return (
         <View style={styles.container}>
             <StatusBar />
             <Text style={styles.eventName}>Nome do evento</Text>
             <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022</Text>
 
-            <TextInput
-                style={styles.input}
-                placeholder="Nome do participante"
-                placeholderTextColor={"#6b6b6b"}
-            />
+            <View style={styles.form}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Nome do participante"
+                    placeholderTextColor={"#6b6b6b"}
+                />
+
+                <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+                    <Text style={styles.buttonText}>
+                        +
+                    </Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
